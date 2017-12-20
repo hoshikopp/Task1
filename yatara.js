d3c5dfbc5e1c6838a6b172279fe835d15100bc07
HOSHIKOPP <hoshi.chikako@crunchstyle.jp>
2017-12-20 21:02:59 +0900

    go

--- yatara.js
+++ yatara.js
@@ -1,6 +1,7 @@
 $(function(){
   $('.tab li').click(function(){
     $(this).addClass('select');
+    $(this).siblings('li').removeClass('select');
     $('section').addClass('hide');
     const index = $('.tab li').index($(this))
     $('section').eq(index).removeClass('hide');
